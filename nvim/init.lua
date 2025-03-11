028bede826a50b0e8671e955f13a165218871b3d
robvandersluis <Robvdsluis@outlook.com>
2025-03-11 15:31:40 +0100

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -169,7 +169,11 @@ vim.keymap.set("n", "Q", "<Nop>")
 vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
+-- paste over selected text, keep the same buffer
 vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- exit all no save
+vim.keymap.set("n", "<leader>x", ":qa!<CR>")
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
@@ -187,10 +191,10 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- TIP: Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+--vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+--vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+--vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+--vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows

99ed17dee70e4519cba416c2e1dd21c479183ae7
robvandersluis <Robvdsluis@outlook.com>
2025-03-23 18:11:28 +0100

    Voeg standaard keymap toe voor het genereren van commitberichten met AI :werkt

--- nvim/lua/plugins/localplugins.lua
+++ nvim/lua/plugins/localplugins.lua
@@ -6,6 +6,8 @@ return {
 				model = "gpt-4o-mini",
 				--		api_key = os.getenv("OPENAI_API_KEY"),
 			})
+			-- Default keymap
+			vim.keymap.set("n", "<leader>gc", "<cmd>GenerateCommitMessage<CR>", { desc = "AI Commit", silent = true })
 		end,
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},

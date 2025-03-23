bbde4cf2b0d25ec2f7a949e9a636a0941b863ec4
robvandersluis <Robvdsluis@outlook.com>
2025-03-23 18:54:20 +0100

    Refactor: update gitcommit-ai plugin configuration to use local directory and add system prompt

--- nvim/lua/plugins/gitcommit-ai.lua
+++ nvim/lua/plugins/gitcommit-ai.lua
@@ -1,9 +1,18 @@
 return {
 	{
-		"robvandersluis/gitcommit-ai.nvim",
+		dir = "~/plugins/gitcommit-ai.nvim",
+		--	"robvandersluis/gitcommit-ai.nvim",
 		config = function()
 			require("gitcommit").setup({
 				model = "gpt-4o-mini",
+				system_prompt = [[
+Je bent een assistent die alleen git commit messages genereert.
+Gebruik een van de volgende prefixen:
+- Feature: voor nieuwe functionaliteit
+- Bugfix: voor opgeloste bugs
+- Refactor: voor codewijzigingen zonder functionele impact
+Geef alleen de commit message terug, zonder uitleg.
+]],
 				--		api_key = os.getenv("OPENAI_API_KEY"),
 			})
 			-- Default keymap

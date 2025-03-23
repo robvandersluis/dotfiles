e371890d792ad8f2119095e90c3c1bc270b4652d
robvandersluis <Robvdsluis@outlook.com>
2025-03-23 13:13:53 +0100

    Feature: Voeg configuratie toe voor gitcommit-ai plugin met gpt-4o-mini model.

--- nvim/lua/plugins/localplugins.lua
+++ nvim/lua/plugins/localplugins.lua
@@ -1,3 +1,11 @@
 return {
-	{ dir = "~/plugins/gitcommit-ai.nvim" },
+	{
+		dir = "~/plugins/gitcommit-ai.nvim",
+		config = function()
+			require("gitcommit").setup({
+				model = "gpt-4o-mini",
+				--		api_key = os.getenv("OPENAI_API_KEY"),
+			})
+		end,
+	},
 }

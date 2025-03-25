47744c50a01fe08a3b55f535713366c4802cb781
robvandersluis <Robvdsluis@outlook.com>
2025-03-25 01:16:24 +0100

    Refactor: Update plugin directory configuration for gitcommit-ai.nvim

--- nvim/lua/plugins/gitcommit-ai.lua
+++ nvim/lua/plugins/gitcommit-ai.lua
@@ -1,12 +1,11 @@
 return {
 	{
-		dir = "~/plugins/gitcommit-ai.nvim",
-		--		"robvandersluis/gitcommit-ai.nvim",
+		--		dir = "~/plugins/gitcommit-ai.nvim",
+		"robvandersluis/gitcommit-ai.nvim",
 		config = function()
 			require("gitcommit").setup({
 				stage_all = false,
 			})
-			-- Default keymap
 			vim.keymap.set("n", "<leader>gc", "<cmd>GenerateCommitMessage<CR>", { desc = "AI Commit", silent = true })
 		end,
 		dependencies = { "nvim-lua/plenary.nvim" },

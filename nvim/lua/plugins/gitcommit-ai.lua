79fa8dff62156f337581aff8ef9087d8454129f3
robvandersluis <Robvdsluis@outlook.com>
2025-03-25 01:12:52 +0100

    Refactor: update gitcommit-ai configuration to remove unnecessary prompts and set stage_all to false

--- nvim/lua/plugins/gitcommit-ai.lua
+++ nvim/lua/plugins/gitcommit-ai.lua
@@ -4,19 +4,7 @@ return {
 		--		"robvandersluis/gitcommit-ai.nvim",
 		config = function()
 			require("gitcommit").setup({
-				-- default settings
-				model = "gpt-4o-mini",
-				system_prompt = [[
-You are an assistant that only generates git commit messages.
-Use one of the following prefixes:
-- Feature: for new functionality
-- Bugfix: for resolved bugs
-- Refactor: for code changes without functional impact
-Return only the commit message, without any explanation.
-]],
-				user_prompt = "Generate a clear commit message based on this git diff:",
-
-				api_key = os.getenv("OPENAI_API_KEY"),
+				stage_all = false,
 			})
 			-- Default keymap
 			vim.keymap.set("n", "<leader>gc", "<cmd>GenerateCommitMessage<CR>", { desc = "AI Commit", silent = true })
